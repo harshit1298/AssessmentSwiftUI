c9a27fc4893e7e61948ea55887662219b7b109da
Harshit Bansal <harshitbansal@countrydelight.in>
2024-12-20 22:54:31 +0530

    Initial Commit

---
+++ DemoApplication_SwiftUI/DemoApplication_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApplication_SwiftUIApp.swift
+//  DemoApplication_SwiftUI
+//
+//  Created by Harshit Bansal on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApplication_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
